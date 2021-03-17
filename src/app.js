18b1bc12d9ddcf4fcdf16fe8897cdb1f39ca6ce4
anafranama <anafranama@gmail.com>
2021-03-17 03:33:21 +0000

    primer

--- src/app.js
+++ src/app.js
@@ -9,3 +9,22 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let simbolos = ["\u2660", "\u2663", "\u2665", "\u2666"];
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let colores = ["red", "black"];
+
+let simboloSeleccionado = simbolos[Math.floor(Math.random() * simbolos.length)];
+let numeroseleccionado = numeros[Math.floor(Math.random() * numeros.length)];
+let colorseleccionado = colores[Math.floor(Math.random() * colores.length)];
+
+console.log(simboloSeleccionado, numeroseleccionado, colorseleccionado);
+
+document.getElementById("corazon1").innerHTML = simboloSeleccionado;
+document.getElementById("corazon1").style.color = colorseleccionado;
+
+document.getElementById("numerocentral").innerHTML = numeroseleccionado;
+document.getElementById("numerocentral").style.color = "black";
+
+document.getElementById("corazon2").innerHTML = simboloSeleccionado;
+document.getElementById("corazon2").style.color = colorseleccionado;
